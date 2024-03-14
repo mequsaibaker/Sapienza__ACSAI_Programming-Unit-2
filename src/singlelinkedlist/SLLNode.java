8c5e1b354b2f7d07e8d2dc86a396d8a4d3ee14ce
me-qusaibaker <me.qusaibaker@gmail.com>
2024-03-14 10:41:54 +0100

    implementing SLLNode class

--- src/singlelinkedlist/SLLNode.java
+++ src/singlelinkedlist/SLLNode.java
@@ -2,4 +2,33 @@ package singlelinkedlist;
 
 public class SLLNode {
     private int value;
+    private SLLNode next;
+
+    //Constructers
+    protected SLLNode(int value) {
+        this.value = value;
+    }
+
+    protected SLLNode(int value, SLLNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    //Setters and Getters
+    public int getValue() {
+        return value;
+    }
+
+    protected void setValue(int value) {
+        this.value = value;
+    }
+
+    public SLLNode getNext() {
+        return next;
+    }
+
+    protected void setNext(SLLNode next) {
+        this.next = next;
+    }
+
 }
