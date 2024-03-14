f838690265cee3898daca2617aa35c2f44ba043f
me-qusaibaker <me.qusaibaker@gmail.com>
2024-03-14 10:47:02 +0100

    updating SLList class

--- src/singlelinkedlist/SLList.java
+++ src/singlelinkedlist/SLList.java
@@ -4,4 +4,15 @@ public class SLList {
     private int len;
     private SLLNode head;
     private SLLNode tail;
+
+    //Constructors
+    public SLList (SLLNode head) {
+        
+    }
+
+    //Add methods
+    public void addHead(SLLNode n) {
+        n.setNext(head);
+        head = n;
+    }
 }
