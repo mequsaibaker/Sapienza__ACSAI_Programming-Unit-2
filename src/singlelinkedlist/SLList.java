f546bc04e34a6fc8ed57a2268015eef4c0a26e2e
me-qusaibaker <me.qusaibaker@gmail.com>
2024-03-14 18:03:49 +0100

    edititng code

--- src/singlelinkedlist/SLList.java
+++ src/singlelinkedlist/SLList.java
@@ -1,5 +1,7 @@
 package singlelinkedlist;
 
+import java.util.EmptyStackException;
+
 public class SLList {
     private final int IND_LOWER_BOUND = 0;
     private int len;
@@ -37,6 +39,13 @@ public class SLList {
         }
     }
 
+    public void addHead(int[] v_list) {
+        SLList list = new SLList(v_list);
+        list.getTail().setNext(head);
+        head = list.getHead();
+        this.len += list.len;
+    }
+
     public void addTail(int v) {
         SLLNode n = new SLLNode(v);
         if (len == 0) {
@@ -46,6 +55,13 @@ public class SLList {
             this.tail = n;
             ++this.len;
         }
+    }
+
+    public void addTail(int[] v_list) {
+        SLList list = new SLList(v_list);
+        tail.setNext(list.getHead());
+        tail = list.getTail();
+        this.len += list.len;
     }
 
     public void insert(int v, int ind) {
@@ -58,6 +74,7 @@ public class SLList {
             SLLNode n = new SLLNode(v);
             n.setNext(new_pre.getNext());
             new_pre.setNext(n);
+            ++len;
         }
     }
 
@@ -68,8 +85,47 @@ public class SLList {
     }
 
     //Remove methods
-    public void removeHead() {
-        
+    public void removeHead() throws EmptyStackException {
+        if (len == 0) {
+            throw new EmptyStackException();
+        }
+        if (len == 1 | len == 2) {
+            head = head.getNext();
+            tail = head;
+            --len;
+        } else {
+            head = head.getNext();
+            --len;
+        }
+    }
+
+    public void removeTail() throws EmptyStackException {
+        if (len == 0) {
+            throw new EmptyStackException();
+        }
+        else if (len == 1) {
+            removeHead();
+        }
+        else if (len == 2) {
+            tail = head;
+            --len;
+        } else {
+            SLLNode n = getNode(len - 2);
+            n.setNext(null);
+            tail = n;
+            --len;
+        }
+    }
+    public void remove(int ind) {
+        if (ind == 0) {
+            removeHead();
+        } else if (ind == len - 1) {
+            removeTail();
+        } else if (ind == 1) {
+            SLLNode n = getNode(ind);
+            SLLNode pre_n = getNode(ind - 1);
+            pre_n.setNext(n.getNext());
+        }
     }
 
     //Search methods
@@ -92,6 +148,22 @@ public class SLList {
         return getNode(start_n.getNext(), --ind, len, ind_lower_bound);
     }
 
+    //View methods
+    @Override
+    public String toString() {
+        return toString(head);
+    }
+
+    private String toString(SLLNode start_n) {
+        String result = "";
+        if (start_n.getNext() == null) {
+            result += start_n.getValue() + " ";
+        } else {
+            result += start_n.getValue() + " " + toString(start_n.getNext());
+        }
+        return result;
+    }
+
     //Setters and Getters
     public int getLen() {
         return len;
@@ -105,7 +177,7 @@ public class SLList {
         return head;
     }
 
-    private void setHead(SLLNode head) {
+    public void setHead(SLLNode head) {
         this.head = head;
     }
 
@@ -113,7 +185,28 @@ public class SLList {
         return tail;
     }
 
-    private void setTail(SLLNode tail) {
+    public void setTail(SLLNode tail) {
         this.tail = tail;
+    }
+
+    public static void main(String[] args) {
+        SLList list = new SLList(new int[]{1,2,3,4,5});
+        System.out.println(list.len +": " + list.toString());
+        list.addHead(10);
+        System.out.println(list.len +": " + list.toString());
+        list.addTail(20);
+        System.out.println(list.len +": " + list.toString());
+        list.insert(50, 3);
+        System.out.println(list.len +": " + list.toString());
+        list.addHead(new int[]{41, 42, 43, 44});
+        System.out.println(list.len +": " + list.toString());
+        list.addTail(new int[]{99, 88, 77, 66});
+        System.out.println(list.len +": " + list.toString());
+        list.removeHead();
+        list.removeHead();
+        System.out.println(list.len +": " + list.toString());
+        list.removeTail();
+        list.removeTail();
+        System.out.println(list.len +": " + list.toString());
     }
 }
